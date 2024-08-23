497ba9b76d822e12291b5eb0f58f3f8e6f2836da
Singharat (Sodality) <151146338+singharat4@users.noreply.github.com>
2024-08-23 16:14:46 +0700

    method put user updated!

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,91 +1,117 @@
-import express, { Router, Request, Response } from 'express';
-import { validateAuthLoginBody, validateAuthRegisterBody } from '../middleware/validateAuth';
-import jwt from 'jsonwebtoken';
-import condb from '../utils/connectDB';
-import bcrypt from 'bcrypt';
-import { UserModel } from '../models/user';
+import express, { Router, Request, Response } from "express";
+import {
+  validateAuthLoginBody,
+  validateAuthRegisterBody,
+} from "../middleware/validateAuth";
+import jwt from "jsonwebtoken";
+import condb from "../utils/connectDB";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/user";
 
 const router: Router = express.Router();
 const secret = process.env.JWT_SECRET || "Enigma";
 
-router.post('/register', validateAuthRegisterBody, (req: Request, res: Response) => {
-  const userData: UserModel | any = req.body;
+router.post(
+  "/register",
+  validateAuthRegisterBody,
+  (req: Request, res: Response) => {
+    const userData: UserModel | any = req.body;
 
-  condb.query(
-    'SELECT * FROM users WHERE phone = ?',
-    [userData.phone],
-    (error: any, existingUsers: any) => {
-      if (error) {
-        console.error('Error checking existing user:', error);
-        return res.status(500).json({ message: 'An error occurred during registration' });
-      }
-
-      if (Array.isArray(existingUsers) && existingUsers.length > 0) {
-        return res.status(400).json({ message: 'Phone number already exists' });
-      }
-
-      bcrypt.hash(userData.password!, 10, (err, hashedPassword) => {
-        if (err) {
-          console.error('Error hashing password:', err);
-          return res.status(500).json({ message: 'An error occurred during registration' });
+    condb.query(
+      "SELECT * FROM users WHERE phone = ?",
+      [userData.phone],
+      (error: any, existingUsers: any) => {
+        if (error) {
+          console.error("Error checking existing user:", error);
+          return res
+            .status(500)
+            .json({ message: "An error occurred during registration" });
         }
 
-        condb.query(
-          'INSERT INTO users (phone, first_name, last_name, password) VALUES (?, ?, ?, ?)',
-          [userData.phone, userData.first_name, userData.last_name, hashedPassword],
-          (error: any, result: any) => {
-            if (error) {
-              console.error('Error inserting user:', error);
-              return res.status(500).json({ message: 'An error occurred during registration' });
-            }
+        if (Array.isArray(existingUsers) && existingUsers.length > 0) {
+          return res
+            .status(400)
+            .json({ message: "Phone number already exists" });
+        }
 
-            delete userData.password;
-            delete userData.password_confirmation;
-
-            if ('insertId' in result) {
-              const userId = result.insertId;
-              const token = jwt.sign({ id: userId, ...userData }, secret, { expiresIn: process.env.JWT_EXPIRE });
-              res.status(201).json({
-                message: 'Registered successfully',
-                data: userData,
-                token
-              });
-            } else {
-              res.status(500).json({ message: 'Failed to insert user' });
-            }
+        bcrypt.hash(userData.password!, 10, (err, hashedPassword) => {
+          if (err) {
+            console.error("Error hashing password:", err);
+            return res
+              .status(500)
+              .json({ message: "An error occurred during registration" });
           }
-        );
-      });
-    }
-  );
-});
 
-router.post('/login', validateAuthLoginBody, (req: Request, res: Response) => {
+          condb.query(
+            "INSERT INTO users (phone, first_name, last_name, password) VALUES (?, ?, ?, ?)",
+            [
+              userData.phone,
+              userData.first_name,
+              userData.last_name,
+              hashedPassword,
+            ],
+            (error: any, result: any) => {
+              if (error) {
+                console.error("Error inserting user:", error);
+                return res
+                  .status(500)
+                  .json({ message: "An error occurred during registration" });
+              }
+
+              delete userData.password;
+              delete userData.password_confirmation;
+
+              if ("insertId" in result) {
+                const userId = result.insertId;
+                const token = jwt.sign({ id: userId, ...userData }, secret, {
+                  expiresIn: process.env.JWT_EXPIRE,
+                });
+                res.status(201).json({
+                  message: "Registered successfully",
+                  data: userData,
+                  token,
+                });
+              } else {
+                res.status(500).json({ message: "Failed to insert user" });
+              }
+            }
+          );
+        });
+      }
+    );
+  }
+);
+
+router.post("/login", (req: Request, res: Response) => {
   const { phone, password } = req.body;
 
   condb.query(
-    'SELECT * FROM users WHERE phone = ?',
+    "SELECT * FROM users WHERE phone = ?",
     [phone],
     (error: any, users: any) => {
       if (error) {
-        console.error('Error finding user:', error);
-        return res.status(500).json({ message: 'An error occurred during login' });
+        console.error("Error finding user:", error);
+        return res
+          .status(500)
+          .json({ message: "An error occurred during login" });
       }
 
       if (!Array.isArray(users) || users.length === 0) {
-        return res.status(401).json({ message: 'Invalid credentials' });
+        return res.status(401).json({ message: "Invalid credentials" });
       }
 
       const user = users[0];
 
       bcrypt.compare(password, user.password, (err, isMatch) => {
         if (err) {
-          console.error('Error comparing passwords:', err);
-          return res.status(500).json({ message: 'An error occurred during login' });
+          console.error("Error comparing passwords:", err);
+          return res
+            .status(500)
+            .json({ message: "An error occurred during login" });
         }
 
         if (!isMatch) {
-          return res.status(401).json({ message: 'Invalid credentials' });
+          return res.status(401).json({ message: "Invalid credentials" });
         }
 
         const userData = {
@@ -93,15 +119,17 @@ router.post('/login', validateAuthLoginBody, (req: Request, res: Response) => {
           phone: user.phone,
           first_name: user.first_name,
           last_name: user.last_name,
-          role: user.role
+          role: user.role,
         };
 
-        const token = jwt.sign(userData, secret, { expiresIn: process.env.JWT_EXPIRE });
+        const token = jwt.sign(userData, secret, {
+          expiresIn: process.env.JWT_EXPIRE,
+        });
 
         res.json({
-          message: 'Logged in successfully',
+          message: "Logged in successfully",
           data: userData,
-          token
+          token,
         });
       });
     }
--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,97 +1,117 @@
-import express, { Router, Request, Response } from 'express';
-import condb from '../utils/connectDB';
-import { MysqlError } from 'mysql';
-
+import express, { Router, Request, Response } from "express";
+import condb from "../utils/connectDB";
+import { MysqlError } from "mysql";
 
 const router: Router = express.Router();
 
 // GET all users
-router.get('/', (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
   try {
-    condb.query('SELECT * FROM users', (err: MysqlError | null, results: any) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal Server Error' });
-      } else {
-        res.json({ message: 'GET all users', data: results });
+    condb.query(
+      "SELECT * FROM users",
+      (err: MysqlError | null, results: any) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ message: "Internal Server Error" });
+        } else {
+          res.json({ message: "GET all users", data: results });
+        }
       }
-    });
+    );
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
 // GET a specific user
-router.get('/:id', (req: Request, res: Response) => {
+router.get("/:id", (req: Request, res: Response) => {
   const userId = req.params.id;
   try {
-    condb.query('SELECT * FROM users WHERE id = ?', [userId], (err: MysqlError | null, results: any) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal Server Error' });
-      } else if (results.length === 0) {
-        res.status(404).json({ message: `User with ID ${userId} not found` });
-      } else {
-        res.json({ message: `GET user with id ${userId}`, data: results[0] });
+    condb.query(
+      "SELECT * FROM users WHERE id = ?",
+      [userId],
+      (err: MysqlError | null, results: any) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ message: "Internal Server Error" });
+        } else if (results.length === 0) {
+          res.status(404).json({ message: `User with ID ${userId} not found` });
+        } else {
+          res.json({ message: `GET user with id ${userId}`, data: results[0] });
+        }
       }
-    });
+    );
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
 // PUT update a user
-router.put('/:id', (req: Request, res: Response) => {
-  const lottoId = req.params.id;
-  const lottoData = req.body;
+router.put("/:id", (req: Request, res: Response) => {
+  const userId = req.params.id;
+  const userData = req.body;
   try {
-    condb.query('SELECT * FROM lottos WHERE id = ?', [lottoId], (err: MysqlError | null, results: any) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal Server Error' });
-      } else if (results.length === 0) {
-        res.status(404).json({ message: `Lotto with ID ${lottoId} not found` });
-      } else {
-        const updatedLottoData = { ...results[0], ...lottoData };
-        condb.query('UPDATE lottos SET ? WHERE id = ?', [updatedLottoData, lottoId], (updateErr: MysqlError | null, updateResults: any) => {
-          if (updateErr) {
-            console.error(updateErr);
-            res.status(500).json({ message: 'Internal Server Error' });
-          } else {
-            res.json({ 
-              message: `Updated lotto with id ${lottoId}`, 
-              data: updatedLottoData 
-            });
-          }
-        });
+    condb.query(
+      "SELECT * FROM users WHERE id = ?",
+      [userId],
+      (err: MysqlError | null, results: any) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ message: "Internal Server Error" });
+        } else if (results.length === 0) {
+          res.status(404).json({ message: `User with ID ${userId} not found` });
+        } else {
+          delete userData.role;
+          delete userData.wallet;
+          delete userData.password;
+          const updatedUserData = { ...results[0], ...userData };
+          condb.query(
+            "UPDATE users SET ? WHERE id = ?",
+            [updatedUserData, userId],
+            (updateErr: MysqlError | null, updateResults: any) => {
+              if (updateErr) {
+                console.error(updateErr);
+                res.status(500).json({ message: "Internal Server Error" });
+              } else {
+                res.json({
+                  message: `Updated user with id ${userId}`,
+                  data: updatedUserData,
+                });
+              }
+            }
+          );
+        }
       }
-    });
+    );
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 // DELETE a user
-router.delete('/:id', (req: Request, res: Response) => {
+router.delete("/:id", (req: Request, res: Response) => {
   const id = req.params.id;
   try {
-    condb.query('DELETE FROM lottos WHERE id = ?', [id], (err: MysqlError | null, result: any) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal Server Error' });
-      } else if (result.affectedRows === 0) {
-        res.status(404).json({ message: 'Lotto not found' });
-      } else {
-        res.json({ message: `Lotto with ID ${id} deleted` });
+    condb.query(
+      "DELETE FROM lottos WHERE id = ?",
+      [id],
+      (err: MysqlError | null, result: any) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ message: "Internal Server Error" });
+        } else if (result.affectedRows === 0) {
+          res.status(404).json({ message: "Lotto not found" });
+        } else {
+          res.json({ message: `Lotto with ID ${id} deleted` });
+        }
       }
-    });
+    );
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
-
 
 export default router;
